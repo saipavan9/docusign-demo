7633410eb551a21640042c3af4173bf07478cc03
Sai Pavan <Sai Pavan@DESKTOP-D67B5VP>
2020-04-30 17:17:55 +0530

    initial commit

---
+++ src/main/java/demo/DocusignDemoApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocusignDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocusignDemoApplication.class, args);
+	}
+
+}
